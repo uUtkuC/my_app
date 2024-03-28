134421367b2ae465dafee238126afe8eec52e6f0
uUtkuC <utkuce7@gmail.com>
2024-03-28 16:03:23 +0300

    added more tests

--- my-app/src/test/java/TestCompute.java
+++ my-app/src/test/java/TestCompute.java
@@ -25,13 +25,42 @@ public class TestCompute {
   @Test
   public void messageQueueDoesNotContainTest(){
     MessageQueue mq = mock(MessageQueue.class);
+    String s = "not included";
+    mq.contains(s);
+    when(mq.size()).thenReturn(1);
+    when(mq.contains("not included")).thenReturn(false);
     c = new Compute(mq);
 
+  assertTrue(c.countNumberOfOccurrences(s)==0);
 
-    assertTrue(i==-1);
   }
+  @Test
+  public void messageQueueDoesContainTest(){
+    MessageQueue mq = mock(MessageQueue.class);
+    String s = "included";
+    mq.contains(s);
+    when(mq.size()).thenReturn(1);
+    when(mq.contains("included")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("included");
+    c = new Compute(mq);
+    assertTrue(c.countNumberOfOccurrences(s)==1);
+
+  }
+  @Test
+  public void messageQueueDoesContainAtAnotherI(){
+    MessageQueue mq = mock(MessageQueue.class);
+    String s = "included";
+    mq.contains(s);
+    when(mq.size()).thenReturn(2);
+    when(mq.contains("included")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("ahmet");
+    when(mq.getAt(1)).thenReturn("included");
+    c = new Compute(mq);
+
+    assertEquals(2, c.countNumberOfOccurrences("included"));
 
 
+  }
 
 
 
