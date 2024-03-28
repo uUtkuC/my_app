1a5410e49149febd9a4ab93be929915174dedc09
uUtkuC <utkuce7@gmail.com>
2024-03-28 17:00:24 +0300

    100 percent test rate is achieved for all java files

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -30,12 +30,25 @@ public class TestUtil {
     assertTrue(i==false);
   }
   @Test
-  public void oddLengthArgTest(){
-    boolean i = c.compute(1,2);
-    assertTrue(i==false);
+  public void sumIsMultipleOfItsComponents(){
+    boolean i = c.compute(1,2,3);
+
+    assertTrue(i==true);
+
   }
+  @Test
+  public void sumIsUnqiue(){
+    boolean i = c.compute(5,2,4);
+
+    assertTrue(i==false);
+
+  }
+  @Test(expected=RuntimeException.class)
+  public void inputZeroIsEntered(){
+    c.compute(5,2,0);
 
 
+  }
 
 
 }
